cbe33aad94788876e70e41f6588877cb64110cd6
Charles <Charles@FenwickElliott.io>
2017-01-12 10:48:56 -0800

    cp9 refactored

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,27 +1,22 @@
 var points = document.getElementsByClassName('point');
 
-var animate = function() {
-
-	var reveal = function(i){
-		points[i].style.opacity = 1;
-		points[i].style.transform = "scaleX(1) translateY(0)";
-		points[i].style.msTransform = "scaleX(1) translateY(0)";
-		points[i].style.WebkitTransform = "scaleX(1) translate(0)";	
-	};
-	for(var i=0; i< points.length; i++){
-		reveal(i);
-	}
+var animate = function(){
+    for (var i=0; i<points.length; i++){
+        points[i].style.opacity = 1;
+        points[i].style.transform = "scaleX(1) translateY(0)";
+        points[i].style.msTransform = "scaleX(1) translateY(0)";
+        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
 };
 
 window.onload = function() {
 	if (window.innerHeight> 950 ){
 		animate();
 	}	
-		
 	
-	window.addEventListener('scroll', function(event){
+	window.addEventListener('scroll', function(){
 		if (document.documentElement.scrollTop || document.body.scrollTop >= points[0].getBoundingClientRect().top - window.innerHeight + 200) {
              animate();   
          }
 	});
-}
+};
