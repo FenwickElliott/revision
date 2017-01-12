aa9b62554e5b7fcc77da1c18f3a66591dba05333
Charles <Charles@FenwickElliott.io>
2017-01-12 10:34:41 -0800

    cp9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,27 @@
-var animate = function(){
-	var points = document.getElementsByClassName('point');
-	
-	for (var i=0; i<points.length; i++){
+var points = document.getElementsByClassName('point');
+
+var animate = function() {
+
+	var reveal = function(i){
 		points[i].style.opacity = 1;
 		points[i].style.transform = "scaleX(1) translateY(0)";
 		points[i].style.msTransform = "scaleX(1) translateY(0)";
-		points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+		points[i].style.WebkitTransform = "scaleX(1) translate(0)";	
+	};
+	for(var i=0; i< points.length; i++){
+		reveal(i);
 	}
 };
 
-window.onload(funtion{
-	animate();
-}); 
+window.onload = function() {
+	if (window.innerHeight> 950 ){
+		animate();
+	}	
+		
+	
+	window.addEventListener('scroll', function(event){
+		if (document.documentElement.scrollTop || document.body.scrollTop >= points[0].getBoundingClientRect().top - window.innerHeight + 200) {
+             animate();   
+         }
+	});
+}
